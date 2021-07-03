1c09589d88c756efdec04a57102d290f316b80ab
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-04 00:12:24 +0200

    :bug: Fix slicing with negative `start` or `stop` exceeding length (#19)
    * :bug: Fix slicing with negative `start` exceeding length
    
    * :bug: Fix slicing with negative `stop` exceeding length

--- tests/test_lazysequence.py
+++ tests/test_lazysequence.py
@@ -60,11 +60,25 @@ def test_getslice_negative_start() -> None:
     assert 2 == item
 
 
+def test_getslice_negative_start_empty() -> None:
+    """It returns the items at the given positions."""
+    s: lazysequence[int] = lazysequence([])
+    for _ in s[-1:]:
+        pass
+
+
 def test_getslice_negative_stop() -> None:
     """It returns the items at the given positions."""
     s = lazysequence([1, 2])
     [item] = s[:-1]
     assert 1 == item
+
+
+def test_getslice_negative_stop_empty() -> None:
+    """It returns the items at the given positions."""
+    s: lazysequence[int] = lazysequence([])
+    for _ in s[:-1]:
+        pass
 
 
 def test_getslice_negative_step() -> None:
