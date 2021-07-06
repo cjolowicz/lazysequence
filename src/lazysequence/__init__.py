9e1762f5531455054d8391ec56ef629369640171
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-06 16:06:14 +0200

    release: replace inline code with function call to `_slice.astuple`

--- src/lazysequence/__init__.py
+++ src/lazysequence/__init__.py
@@ -187,9 +187,7 @@ class lazysequence(Sequence[_T_co]):  # noqa: N801
             theslice = self._slice.reverse(size)
             return islice(reversed(self._cache), *theslice.astuple())
 
-        return islice(
-            chain(self._cache, self._iter), self._start, self._stop, self._step
-        )
+        return islice(chain(self._cache, self._iter), *self._slice.astuple())
 
     def __bool__(self) -> bool:
         """Return True if there are any items in the sequence."""
