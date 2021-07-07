d81fc5ee241f6460c46e412a49376e7c9c4f4ec3
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-07 22:11:01 +0200

    _getitem: slide statement

--- src/lazysequence/__init__.py
+++ src/lazysequence/__init__.py
@@ -304,10 +304,9 @@ class lazysequence(Sequence[_T_co]):  # noqa: N801
                 self._fill()
                 start = origin.rresolve(start, self._cachesize)
             except IndexError:
+                start = None
                 if origin.start is not None and origin.start > 0:
                     start = origin.start - 1
-                else:
-                    start = None
 
         # determine stop
         if stop is not None:
