dd1101252607e6358d3b41a848f145a6dc3abcf9
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-09 23:37:12 +0200

    inline function `_slice` into `_iterate`

--- src/lazysequence/__init__.py
+++ src/lazysequence/__init__.py
@@ -228,7 +228,10 @@ class lazysequence(Sequence[_T_co]):  # noqa: N801
         return len(self._cache)
 
     def _iterate(self, iterator: Iterator[_T_co]) -> Iterator[_T_co]:
-        slice = self._slice
+        slice = self.__slice
+        if slice.hasnegativebounds():
+            self._fill()
+            slice = slice.withpositivebounds(self._cachesize)
 
         if slice.step < 0:
             self._fill()
